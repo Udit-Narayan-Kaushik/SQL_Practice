d7e3e0553f3db4e66810ffeb2edffcfa717dbe29
Udit_Narayan_Kaushik <155947564+Udit-Narayan-Kaushik@users.noreply.github.com>
2024-11-17 12:26:11 +0530

    Update Basic_Select_2.sql

--- Basic_Select_2.sql
+++ Basic_Select_2.sql
@@ -1,4 +1,7 @@
-Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+-- Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
 
 
-Solution -  SELECT DISTINCT CITY FROM STATION WHERE RIGHT (CITY, 1) IN ('A', 'E', 'I', 'O', 'U');
+-- Solution - 
+  SELECT DISTINCT CITY 
+  FROM STATION 
+  WHERE RIGHT (CITY, 1) IN ('A', 'E', 'I', 'O', 'U');
